577a22001d1e9175a564e515d40afb520d6254da
Namya Shah <namya.shah123@gmail.com>
2023-05-25 17:53:40 +0530

    Brew Files Modified

--- brew.sh
+++ brew.sh
@@ -11,20 +11,18 @@ brew install vim
 brew install mingw-w64
 brew install git
 
+
 # Install MacOS Applications
 brew install --cask vlc
 brew install --cask authy
-brew install --cask pdfelement
+brew install --cask nitro-pdf-editor
 brew install --cask google-chrome
 brew install --cask discord
 brew install --cask todoist
 brew install --cask iina
 brew install --cask notion
 brew install --cask visual-studio-code
-brew install --cask brave-browser
 brew install --cask iterm2
-brew install --cask whatsapp
-brew install --cask telegram
 brew install --cask rectangle
 brew install --cask raycast
 brew install --cask fig
@@ -32,11 +30,7 @@ brew install --cask anydesk
 brew install --cask keka
 brew install --cask android-file-transfer
 brew install --cask android-platform-tools
-brew install --cask app-cleaner
 brew install --cask spotify
 
 # Install Fonts
-brew tap homebrew/cask-fonts
-brew intall --cask font-source-code-pro
-brew install --cask font-hack-nerd-font
 brew install --cask font-jetbrains-mono
