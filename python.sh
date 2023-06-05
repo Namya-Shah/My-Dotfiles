6e026d29d5c46fd262ba3167e90d6dd35a795368
Namya Shah <namya.shah123@gmail.com>
2023-06-05 21:09:14 +0530

    Modified Python Script

--- python.sh
+++ python.sh
@@ -1,5 +1,5 @@
 #Python Script
-
+#!/bin/zsh
 pip install tensorflow
 pip install pandas
 pip install numpy
@@ -14,3 +14,15 @@ pip install pytorch
 pip install Scrapy3
 pip install beautifulsoup4
 pip install selenium
+pip install pygame
+pip install pyautogui
+pip install pyttsx3
+pip install seaborn
+pip install spacy
+pip install NLTK
+pip install django
+pip install flask
+pip install kivy
+pip install pillow
+pip install pywhatkit
+pip install turtle
